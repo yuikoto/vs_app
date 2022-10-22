706abcd74468056b583ad8244ec655ed98f2a806
yuikoto <soraifu.com@gmail.com>
2022-10-22 18:08:12 +0900

    一覧画面の完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -17,7 +17,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        return view('posts.index');
+        $posts = Post::with('user')->latest()->paginate(4);
+
+        return view('posts.index', compact('posts'));
     }
 
     /**
